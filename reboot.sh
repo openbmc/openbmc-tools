8dfebd8dc49d22ecac400e0cc29ca1cc1e3dce98
Andrew Jeffery <andrew@aj.id.au>
2017-03-30 14:56:54 +1030

    OpenBMC reboot loop for POWER hosts
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

---
+++ reboot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+CONNECT=$1
+
+i=0
+
+while true;
+do
+    echo Boot $i
+
+    ssh ${CONNECT} /usr/sbin/obmcutil poweron
+    time expect petitboot.exp ${CONNECT}
+    ssh ${CONNECT} /usr/sbin/obmcutil poweroff
+    sleep 20 # mboxd reload period. Remove when protocol v2 daemon is integrated
+
+    i=$(($i + 1))
+
+    echo
+done
+
