724782e043d08837a64351453c842a8d3aba93db
Andrew Jeffery <andrew@aj.id.au>
2017-03-30 19:41:09 +1030

    Allow options to be passed to ssh
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- reboot.sh
+++ reboot.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-CONNECT=$1
+CONNECT="$@"
 
 i=0
 
@@ -11,7 +11,7 @@ do
     echo Boot $i
 
     ssh ${CONNECT} /usr/sbin/obmcutil poweron
-    time expect petitboot.exp ${CONNECT}
+    time expect petitboot.exp -- ${CONNECT}
     ssh ${CONNECT} /usr/sbin/obmcutil poweroff
     sleep 20 # mboxd reload period. Remove when protocol v2 daemon is integrated
 
