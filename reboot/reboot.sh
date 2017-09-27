d5bbddc6a876cc8b9e181f9c98fdf6f7655898fe
Andrew Jeffery <andrew@aj.id.au>
2017-09-27 20:38:33 +0930

    reboot: Add directory, move scripts
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- reboot/reboot.sh
+++ reboot/reboot.sh
@@ -13,7 +13,6 @@ do
     ssh ${CONNECT} /usr/sbin/obmcutil poweron
     time expect petitboot.exp -- ${CONNECT}
     ssh ${CONNECT} /usr/sbin/obmcutil poweroff
-    sleep 20 # mboxd reload period. Remove when protocol v2 daemon is integrated
 
     i=$(($i + 1))
 
