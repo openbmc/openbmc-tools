93854254583ac3f522c1dfa70da85adc90fb350d
Matt Spinler <spinler@us.ibm.com>
2018-09-04 16:24:06 -0500

    Update openbmctool version to 1.07
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- thalerj/openbmctool.py
+++ thalerj/openbmctool.py
@@ -2258,7 +2258,7 @@ def main(argv=None):
          main function for running the command line utility as a sub application  
     """ 
     global toolVersion 
-    toolVersion = "1.06"
+    toolVersion = "1.07"
     parser = createCommandParser()
     args = parser.parse_args(argv)
         
