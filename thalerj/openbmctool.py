e05e281163d7ce54e1f8eabe73b72ab55f81427e
Nagaraju Goruganti <ngorugan@in.ibm.com>
2018-11-13 06:09:29 -0600

    openbmctool: add list command
    Add following command to listout all configured properties.
     -list
    
    Change-Id: I7e2b7a3bbc7d348f5658284203c459a8cd059a3b
    Signed-off-by: Nagaraju Goruganti <ngorugan@in.ibm.com>

--- thalerj/openbmctool.py
+++ thalerj/openbmctool.py
@@ -2271,6 +2271,30 @@ def disableLDAP(host, args, session):
     return res.text
 
 
+def listLDAP(host, args, session):
+    """
+         Called by the ldap function. Prints out all LDAP's configured properties
+
+         @param host: string, the hostname or IP address of the bmc
+         @param args: contains additional arguments used by the ldap subcommand
+         @param session: the active session to use
+         @param args.json: boolean, if this flag is set to true, the output
+                will be provided in json format for programmatic consumption
+         @return returns total LDAP's configured properties.
+    """
+    url="https://"+host+"/xyz/openbmc_project/user/ldap/enumerate"
+    httpHeader = {'Content-Type':'application/json'}
+    try:
+        res = session.get(url, headers=httpHeader, verify=False, timeout=40)
+    except(requests.exceptions.Timeout):
+        return(connectionErrHandler(args.json, "Timeout", None))
+    except(requests.exceptions.ConnectionError) as err:
+        return connectionErrHandler(args.json, "ConnectionError", err)
+    except(requests.exceptions.RequestException) as err:
+        return connectionErrHandler(args.json, "RequestException", err)
+    return res.text
+
+
 def localUsers(host, args, session):
     """
         Enables and disables local BMC users.
@@ -2564,6 +2588,9 @@ def createCommandParser():
     # disable LDAP
     parser_disable_ldap = ldap_sub.add_parser("disable", help="disables the LDAP")
     parser_disable_ldap.set_defaults(func=disableLDAP)
+    # list LDAP
+    parser_ldap_config = ldap_sub.add_parser("list", help="prints out a list of all LDAP's configured properties")
+    parser_ldap_config.set_defaults(func=listLDAP)
 
     return parser
 
