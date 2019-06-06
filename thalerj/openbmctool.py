d6b60bf184c251952ec8a1d33ca30f0646cc57db
Marri Devender Rao <devenrao@in.ibm.com>
2019-06-06 04:54:22 -0500

    Add support for certificate update command
    Existing REST server is broken with design changes
    done to phosphor-certificate-manager as it is catered
    for Redfish specification
    
    Modified certificate update command to use Redfish
    specification
    
    Tested:
    bash-4.2$ python openbmctool.py -H w37 -U xxx -P xxxx certificate update
    server -f testcert.pem
    {
      "@odata.context": "/redfish/v1/$metadata#Certificate.Certificate",
      "@odata.id": "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates/1",
      "@odata.type": "#Certificate.v1_0_0.Certificate",
      "CertificateString": "-----BEGIN CERTIFICATE----DzyQ==\n-----END
    CERTIFICATE-----\n",
      "Description": "HTTPS Certificate",
      "Id": "1",
      "Issuer": {
        "City": "SomeCity",
        "CommonName": "www.company.com",
        "Country": "US",
        "Organization": "MyCompany",
        "OrganizationalUnit": "MyDivision",
        "State": "VA"
      },
      "KeyUsage": [
        "KeyAgreement",
        "ServerAuthentication"
      ],
      "Name": "HTTPS Certificate",
      "Subject": {
        "City": "SomeCity",
        "CommonName": "www.company.com",
        "State": "VA"
      },
      "ValidNotAfter": "2029-03-14T02:11:02+00:00",
      "ValidNotBefore": "2019-03-17T02:11:02+00:00"
    }
    
    Change-Id: I3502683f7bf22898826accb2d154da3b9e6744b1
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- thalerj/openbmctool.py
+++ thalerj/openbmctool.py
@@ -2512,6 +2512,18 @@ def remoteLoggingConfig(host, args, session):
         return(connectionErrHandler(args.json, "Timeout", None))
     return res.text
 
+def redfishSupportPresent(host, session):
+    url = "https://" + host + "/redfish/v1"
+    try:
+        resp = session.get(url, headers=jsonHeader, verify=False, timeout=baseTimeout)
+    except(requests.exceptions.Timeout):
+        return False
+    except(requests.exceptions.ConnectionError) as err:
+        return False
+    if resp.status_code != 200:
+        return False
+    else:
+       return True
 
 def certificateUpdate(host, args, session):
     """
@@ -2524,24 +2536,46 @@ def certificateUpdate(host, args, session):
          @param args: contains additional arguments used by the certificate update sub command
          @param session: the active session to use
     """
-
     httpHeader = {'Content-Type': 'application/octet-stream'}
     httpHeader.update(xAuthHeader)
-    url = "https://" + host + "/xyz/openbmc_project/certs/" + args.type.lower() + "/" + args.service.lower()
-    data = open(args.fileloc, 'rb').read()
-    print("Updating certificate url=" + url)
-    try:
-        resp = session.put(url, headers=httpHeader, data=data, verify=False)
-    except(requests.exceptions.Timeout):
-        return(connectionErrHandler(args.json, "Timeout", None))
-    except(requests.exceptions.ConnectionError) as err:
-        return connectionErrHandler(args.json, "ConnectionError", err)
-    if resp.status_code != 200:
-        print(resp.text)
-        return "Failed to update the certificate"
+    if redfishSupportPresent(host, session):
+        url = "";
+        if(args.type.lower() == 'server'):
+            url = "https://" + host + \
+                "/redfish/v1/Managers/bmc/NetworkProtocol/HTTPS/Certificates"
+        elif(args.type.lower() == 'client'):
+            url = "https://" + host + \
+                "/redfish/v1/AccountService/LDAP/Certificates"
+        elif(args.type.lower() == 'authority'):
+            url = "https://" + host + \
+            "/redfish/v1/Managers/bmc/Truststore/Certificates"
+        data = open(args.fileloc, 'rb').read()
+        try:
+            resp = session.post(url, headers=httpHeader, data=data, verify=False)
+        except(requests.exceptions.Timeout):
+            return(connectionErrHandler(args.json, "Timeout", None))
+        except(requests.exceptions.ConnectionError) as err:
+            return connectionErrHandler(args.json, "ConnectionError", err)
+        if resp.status_code != 200:
+            print(resp.text)
+            return "Failed to update the certificate"
+        else:
+           print("Update complete.")
     else:
-       print("Update complete.")
-
+        url = "https://" + host + "/xyz/openbmc_project/certs/" + args.type.lower() + "/" + args.service.lower()
+        data = open(args.fileloc, 'rb').read()
+        print("Updating certificate url=" + url)
+        try:
+            resp = session.put(url, headers=httpHeader, data=data, verify=False)
+        except(requests.exceptions.Timeout):
+            return(connectionErrHandler(args.json, "Timeout", None))
+        except(requests.exceptions.ConnectionError) as err:
+            return connectionErrHandler(args.json, "ConnectionError", err)
+        if resp.status_code != 200:
+            print(resp.text)
+            return "Failed to update the certificate"
+        else:
+            print("Update complete.")
 
 def certificateDelete(host, args, session):
     """
