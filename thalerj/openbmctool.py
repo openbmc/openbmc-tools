934147699c32409c4e6d87a8e80476bccfb6c29c
Sunitha Harish <sunithaharish04@gmail.com>
2019-07-31 03:59:03 -0500

    Fix for the network usecases
    The below network commands are fixed
    1. getDomainName failure
    2. rmIP functionality not working
    3. addVLAN functionality not working
    4. network addIP has the -p option as optional.
       But without this input, the functionality does not work.
    
    Tested by:
    1.python openbmctool.py -U <user> -P <passwd> -H <host> network getDomainName -I eth0
    2.python openbmctool.py -U <user> -P <passwd> -H <host> network rmIP -a <ip> -I eth0
    3.python openbmctool.py -U <user> -P <passwd> -H <host> network addVLAN -I eth0 -n <id>
    4.Made the -p option mandatory for addIP command.
    
    Signed-off-by: Sunitha Harish <sunithaharish04@gmail.com>
    Change-Id: Ie328d04996b0fef048d73f03ba9e41d9d8779776

--- thalerj/openbmctool.py
+++ thalerj/openbmctool.py
@@ -2956,8 +2956,8 @@ def getDomainName(host, args, session):
     except(requests.exceptions.ConnectionError) as err:
         return connectionErrHandler(args.json, "ConnectionError", err)
     if res.status_code == 404:
-        return "The specified Interface"+"("+args.Interface+")"+\
-        " doesn't exist"
+        return "Domain name is not set on the interface"+"("+\
+               args.Interface+")"
 
     return res.text
 
@@ -3334,19 +3334,22 @@ def deleteIP(host, args, session):
         return "No object found for given address on given Interface"
 
     for obj in objDict['data']:
-        if args.address in objDict['data'][obj]['Address']:
-            url = "https://"+host+obj+"/action/delete"
-            try:
-                res = session.post(url, headers=jsonHeader, json=data,
-                                   verify=False, timeout=baseTimeout)
-            except(requests.exceptions.Timeout):
-                return(connectionErrHandler(args.json, "Timeout", None))
-            except(requests.exceptions.ConnectionError) as err:
-                return connectionErrHandler(args.json, "ConnectionError", err)
-            return res.text
-        else:
-            continue
-    return "No object found for given address on given Interface"
+        try:
+            if args.address in objDict['data'][obj]['Address']:
+                url = "https://"+host+obj+"/action/Delete"
+                try:
+                    res = session.post(url, headers=jsonHeader, json=data,
+                                       verify=False, timeout=baseTimeout)
+                except(requests.exceptions.Timeout):
+                    return(connectionErrHandler(args.json, "Timeout", None))
+                except(requests.exceptions.ConnectionError) as err:
+                    return connectionErrHandler(args.json, "ConnectionError", err)
+                return res.text
+            else:
+                continue
+        except KeyError:
+            return "No object found for address " + args.address + \
+                    " on Interface(" + args.Interface + ")"
 
 
 def addVLAN(host, args, session):
@@ -3362,8 +3365,7 @@ def addVLAN(host, args, session):
 
     url = "https://" + host+"/xyz/openbmc_project/network/action/VLAN"
 
-    data = {"data": [args.Interface,args.Identifier]}
-
+    data = {"data": [args.Interface,int(args.Identifier)]}
     try:
         res = session.post(url, headers=jsonHeader, json=data, verify=False,
                            timeout=baseTimeout)
@@ -3372,8 +3374,8 @@ def addVLAN(host, args, session):
     except(requests.exceptions.ConnectionError) as err:
         return connectionErrHandler(args.json, "ConnectionError", err)
     if res.status_code == 400:
-        return "The specified Interface" + "(" + args.Interface + ")" +\
-            " doesn't exist"
+        return "Adding VLAN to interface" + "(" + args.Interface + ")" +\
+            " failed"
 
     return res.text
 
@@ -4382,7 +4384,8 @@ def createCommandParser():
                                   help="The gateway for given interface")
     parser_ip_config.add_argument("-l", "--prefixLength", required=True,
                                   help="The prefixLength of IP address")
-    parser_ip_config.add_argument("-p", "--type", choices=['ipv4', 'ipv6'],
+    parser_ip_config.add_argument("-p", "--type", required=True,
+                                  choices=['ipv4', 'ipv6'],
                                   help="The protocol type of the given"
                                   "IP address")
     parser_ip_config.add_argument("-I", "--Interface", required=True,
