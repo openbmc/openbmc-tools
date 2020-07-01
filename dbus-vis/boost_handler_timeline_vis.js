f3dd74b1e68b4c7fb2f40094cacbb10853808b32
Sui Chen <suichen@google.com>
2020-06-30 18:14:03 -0700

    dbus-vis: Initial commit
    dbus-vis visualizes PCAP files captured by busctl as well as
    text-format outputs from busctl.
    
    dbus-vis displays the visualized results in horizontal
    timelines, where each horizontal bar represents a DBus request, or
    an IPMI request (an IPMI request is a specific kind of DBus request
    that is made on the IPMI daemon.)
    The requests may be grouped by one or more fields including
    type, sender, destination, path, interface, member.
    
    dbus-vis uses a slightly modified dbus-pcap to parse a capgure file.
    The modifications are: 1) trancated packets (either truncated by
    busctl due to size limit for busctl or truncated by scapy due to
    the MTU size limit) are not discarded such that DBus method calls
    that return huge amounts of reply can be correctly displayed.
    2) Timestamps and progress information are printed in JSON mode so
    we only need to run dbus-pcap once for both timestamps and JSON
    dumps.
    
    In addition, if the BMC console is available, dbus-vis can connect
    to the BMC console and capture DBus/IPMI requests in real time.
    
    dbus-vis is in the form of a web page and is wrapped using Electron
    to run as a desktop app.
    
    Signed-off-by: Sui Chen <suichen@google.com>
    Change-Id: I863c2ba80025d10efb44fd12868e37912fae9a47

---
+++ dbus-vis/boost_handler_timeline_vis.js
@@ -0,0 +1,235 @@
+// This script deals with Boost ASIO handlers
+// Will need to add code to visualize one-shot events
+
+// Fields: HandlerNumber, Level,
+// Creation time, Enter time, Exit time, EnterDescription,
+//    [ [Operation, time] ]
+
+// Will use g_StartingSec as the starting of epoch
+
+var ASIO_Data = [];
+var ASIO_Timestamp = [];
+
+function FindFirstEntrySlot(slots) {
+  let i = 0;
+  for (; i < slots.length; i++) {
+    if (slots[i] == undefined) break;
+  }
+  if (i >= slots.length) slots.push(undefined);
+  return i;
+}
+
+function SimplifyDesc(desc) {
+  const idx0 = desc.indexOf('0x');
+  if (idx0 == -1)
+    return desc;
+  else {
+    const d1 = desc.substr(idx0 + 2);
+    let idx1 = 0;
+    while (idx1 + 1 < d1.length &&
+           ((d1[idx1] >= '0' && d1[idx1] <= '9') ||
+            (d1[idx1] >= 'A' && d1[idx1] <= 'F') ||
+            (d1[idx1] >= 'a' && d1[idx1] <= 'f'))) {
+      idx1++;
+    }
+    return desc.substr(0, idx0) + d1.substr(idx1)
+  }
+}
+
+function ParseBoostHandlerTimeline(content) {
+  let parsed_entries = [];
+  const lines = content.split('\n');
+  let slots = [];               // In-flight handlers
+  let in_flight_id2level = {};  // In-flight ID to level
+
+  for (let lidx = 0; lidx < lines.length; lidx++) {
+    const line = lines[lidx];
+    if (line.startsWith('@asio|') == false) continue;
+    const sp = line.split('|');
+
+    const tag = sp[0], ts = sp[1], action = sp[2], desc = sp[3];
+    let handler_id = -999;
+    let ts_sec = parseFloat(ts);
+    const simp_desc = SimplifyDesc(desc);
+
+    if (action.indexOf('*') != -1) {
+      const idx = action.indexOf('*');
+      const handler_id = parseInt(action.substr(idx + 1));
+      const level = FindFirstEntrySlot(slots);
+
+      // Create an entry here
+      let entry = [
+        handler_id, level, ts_sec, undefined, undefined, desc, simp_desc, []
+      ];
+
+      slots[level] = entry;
+      in_flight_id2level[handler_id] = level;
+    } else if (action[0] == '>') {  // The program enters handler number X
+      handler_id = parseInt(action.substr(1));
+      if (handler_id in in_flight_id2level) {
+        const level = in_flight_id2level[handler_id];
+        let entry = slots[level];
+        entry[3] = ts_sec;
+      }
+    } else if (action[0] == '<') {
+      handler_id = parseInt(action.substr(1));
+      if (handler_id in in_flight_id2level) {
+        const level = in_flight_id2level[handler_id];
+        let entry = slots[level];
+        entry[4] = ts_sec;
+        slots[level] = undefined;
+        parsed_entries.push(entry);
+        delete in_flight_id2level[handler_id];
+      }
+    } else if (action[0] == '.') {  // syscalls
+    }
+  }
+
+  console.log(
+      'Boost handler log: ' + parsed_entries.length + ' entries' +
+      ', ' + slots.length + ' levels');
+  ASIO_Data = parsed_entries;
+  return parsed_entries;
+}
+
+function Group_ASIO(preprocessed, group_by) {
+  let grouped = {};
+  const IDXES = {'Layout Level': 1, 'Description': 5, 'Description1': 6};
+  for (var n = 0; n < preprocessed.length; n++) {
+    var key = ''
+    for (var i = 0; i < group_by.length; i++) {
+      if (i > 0) key += ' ';
+      key += ('' + preprocessed[n][IDXES[group_by[i]]]);
+    }
+    if (grouped[key] == undefined) grouped[key] = [];
+    grouped[key].push(preprocessed[n]);
+  }
+  return grouped;
+}
+
+function OnGroupByConditionChanged_ASIO() {
+  var tags = ['bah1', 'bah2', 'bah3'];
+  const v = boost_asio_handler_timeline_view;
+  v.GroupBy = [];
+  v.GroupByStr = '';
+  for (let i = 0; i < tags.length; i++) {
+    let cb = document.getElementById(tags[i]);
+    if (cb.checked) {
+      v.GroupBy.push(cb.value);
+      if (v.GroupByStr.length > 0) {
+        v.GroupByStr += ', ';
+      }
+      v.GroupByStr += cb.value;
+    }
+  }
+  let preproc = ASIO_Data;
+  let grouped = Group_ASIO(preproc, v.GroupBy);
+  GenerateTimeLine_ASIO(grouped);
+  boost_asio_handler_timeline_view.IsCanvasDirty = true;
+}
+
+function GenerateTimeLine_ASIO(grouped) {
+  const keys = Object.keys(grouped);
+  let sortedKeys = keys.slice();
+  let intervals = [];
+  let titles = [];
+
+  const was_starting_time_undefined = (g_StartingSec == undefined);
+
+  for (let i = 0; i < sortedKeys.length; i++) {
+    titles.push({"header":false, "title":sortedKeys[i], "intervals_idxes":[i] });
+    line = [];
+    for (let j = 0; j < grouped[sortedKeys[i]].length; j++) {
+      let entry = grouped[sortedKeys[i]][j];
+      let t0 = parseFloat(entry[3]);
+      let t1 = parseFloat(entry[4]);
+
+      if (was_starting_time_undefined) {
+        if (g_StartingSec == undefined) {
+          g_StartingSec = t0;
+        }
+        g_StartingSec = Math.min(g_StartingSec, t0);
+      }
+
+      line.push([t0, t1, entry, 'ok', 0]);
+    }
+    intervals.push(line);
+  }
+
+  // Time shift
+  for (let i = 0; i < intervals.length; i++) {
+    for (let j = 0; j < intervals[i].length; j++) {
+      let x = intervals[i][j];
+      x[0] -= g_StartingSec;
+      x[1] -= g_StartingSec;
+    }
+  }
+
+  boost_asio_handler_timeline_view.Intervals = intervals.slice();
+  boost_asio_handler_timeline_view.Titles = titles.slice();
+  boost_asio_handler_timeline_view.LayoutForOverlappingIntervals();
+}
+
+// Main view object for Boost handler timeline
+
+boost_asio_handler_timeline_view = new BoostASIOHandlerTimelineView();
+boost_asio_handler_timeline_view.IsCanvasDirty = true;
+
+function draw_timeline_boost_asio_handler(ctx) {
+  boost_asio_handler_timeline_view.Render(ctx);
+}
+
+let Canvas_Asio = document.getElementById('my_canvas_boost_asio_handler');
+
+Canvas_Asio.onmousemove = function(event) {
+  const v = boost_asio_handler_timeline_view;
+  v.MouseState.x = event.pageX - this.offsetLeft;
+  v.MouseState.y = event.pageY - this.offsetTop;
+  if (v.MouseState.pressed == true) {  // Update highlighted area
+    v.HighlightedRegion.t1 = v.MouseXToTimestamp(v.MouseState.x);
+  }
+  v.OnMouseMove();
+  v.IsCanvasDirty = true;
+
+  v.linked_views.forEach(function(u) {
+    u.MouseState.x = event.pageX - Canvas_Asio.offsetLeft;
+    u.MouseState.y = 0;                  // Do not highlight any entry
+    if (u.MouseState.pressed == true) {  // Update highlighted area
+      u.HighlightedRegion.t1 = u.MouseXToTimestamp(u.MouseState.x);
+    }
+    u.OnMouseMove();
+    u.IsCanvasDirty = true;
+  });
+};
+
+Canvas_Asio.onmousedown = function(event) {
+  if (event.button == 0) {
+    boost_asio_handler_timeline_view.OnMouseDown();
+  }
+};
+
+Canvas_Asio.onmouseup = function(event) {
+  if (event.button == 0) {
+    boost_asio_handler_timeline_view.OnMouseUp();
+  }
+};
+
+Canvas_Asio.onwheel = function(event) {
+  event.preventDefault();
+  const v = boost_asio_handler_timeline_view;
+  if (v.IsMouseOverTimeline()) {
+    let dz = 0;
+    if (event.deltaY > 0) {  // Scroll down, zoom out
+      dz = -0.3;
+    } else if (event.deltaY < 0) {  // Scroll up, zoom in
+      dz = 0.3;
+    }
+    v.Zoom(dz, v.MouseXToTimestamp(v.MouseState.x));
+  } else {
+    if (event.deltaY > 0) {
+      v.ScrollY(1);
+    } else if (event.deltaY < 0) {
+      v.ScrollY(-1);
+    }
+  }
+}
---
+++ dbus-vis/dbus_pcap_loader.js
@@ -0,0 +1,323 @@
+// This file performs the file reading step
+// Actual preprocessing is done in dbus_timeline_vis.js
+
+function MyFloatMillisToBigIntUsec(x) {
+  x = ('' + x).split('.');
+  ret = BigInt(x[0]) * BigInt(1000);
+  return ret;
+}
+
+// When the Open File dialog is completed, the name of the opened file will be passed
+// to this routine. Then the program will do the following:
+// 1. Launch "linecount.py" to get the total packet count in the PCAP file for
+//    progress
+// 2. Launch "dbus-pcap" to get the timestamps of each DBus message
+// 3. Launch "dbus-pcap" to get the JSON representation of each DBus message
+//
+function OpenDBusPcapFile(file_name) {
+  // First try to parse using dbus-pcap
+  
+  ShowBlocker('Determining the number of packets in the pcap file ...');
+  const num_lines_py =
+      spawn('python3', ['linecount.py', file_name]);
+  let stdout_num_lines = '';
+  num_lines_py.stdout.on('data', (data) => {
+    stdout_num_lines += data;
+  });
+
+  num_lines_py.on('close', (code) => {
+    let num_packets = parseInt(stdout_num_lines.trim());
+    ShowBlocker('Running dbus-pcap (Pass 1/2, packet timestamps) ...');
+    const dbus_pcap1 =
+        //spawn('python3', ['dbus-pcap', file_name, '--json', '--progress']);
+        spawn('python3', ['dbus-pcap', file_name]);
+    let stdout1 = '';
+    let timestamps = [];
+    let count1 = 0; // In the first phase, consecutive newlines indicate a new entry
+    //const r = new RegExp('([0-9]+/[0-9]+) [0-9]+\.[0-9]+:.*');
+    const r = new RegExp('([0-9]+\.[0-9]+):.*');
+
+    let is_last_newline = false;  // Whether the last scanned character is a newline
+    let last_update_millis = 0;
+    dbus_pcap1.stdout.on('data', (data) => {
+      const s = data.toString();
+      stdout1 += s;
+      for (let i=0; i<s.length; i++) {
+        const ch = s[i];
+        let is_new_line = false;
+        if (ch == '\n' || ch == '\r') {
+          is_new_line = true;
+        }
+        if (!is_last_newline && is_new_line) {
+          count1 ++;
+        }
+        is_last_newline = is_new_line;
+      }
+      const millis = Date.now();
+      if (millis - last_update_millis > 100) { // Refresh at most 10 times per second
+        let pct = parseInt(count1 * 100 / num_packets);
+        ShowBlocker('Running dbus-pcap (Pass 1/2, packet timestamps): ' + count1 + '/' + num_packets + ' (' + pct + '%)');
+        last_update_millis = millis;
+      }
+    });
+
+    dbus_pcap1.on('close', (code) => {
+      ShowBlocker('Running dbus-pcap (Pass 2/2, packet contents) ...');
+      let stdout2 = '';
+      let count2 = 0;
+      is_last_newline = false;
+      const dbus_pcap2 =
+          spawn('python3', ['dbus-pcap', file_name, '--json']);
+      dbus_pcap2.stdout.on('data', (data) => {
+        const s = data.toString();
+        stdout2 += s;
+        for (let i=0; i<s.length; i++) {
+          const ch = s[i];
+          let is_new_line = false;
+          if (ch == '\n' || ch == '\r') {
+            is_new_line = true;
+          }
+          if (!is_last_newline && is_new_line) {
+            count2 ++;
+          }
+          is_last_newline = is_new_line;
+        }
+        const millis = Date.now();
+        if (millis - last_update_millis > 100) { // Refresh at most 10 times per second
+          let pct = parseInt(count2 * 100 / num_packets);
+          ShowBlocker('Running dbus-pcap (Pass 2/2, packet contents): ' + count2 + '/' + num_packets + ' (' + pct + '%)');
+          last_update_millis = millis;
+        }
+      });
+
+      dbus_pcap2.on('close', (code) => {
+        { ShowBlocker('Processing dbus-pcap output ... '); }
+
+        let packets = [];
+        // Parse timestamps
+        let lines = stdout1.split('\n');
+        for (let i=0; i<lines.length; i++) {
+          let l = lines[i].trim();
+          if (l.length > 0) {
+            // Timestamp
+            l = l.substr(0, l.indexOf(':'));
+            const ts_usec = parseFloat(l) * 1000.0;
+            if (!isNaN(ts_usec)) {
+              timestamps.push(ts_usec);
+            } else {
+              console.log('not a number: ' + l);
+            }
+          }
+        }
+
+        // JSON
+        lines = stdout2.split('\n');
+        for (let i=0; i<lines.length; i++) {
+          let l = lines[i].trim();
+          if (l.length > 0) {
+            try {
+              packets.push(JSON.parse(l));
+            } catch (e) {
+              console.log(e);
+            }
+          }
+        }
+
+        Timestamps_DBus = timestamps;
+
+        Data_DBus = packets.slice();
+        OnGroupByConditionChanged_DBus();
+        const v = dbus_timeline_view;
+
+        // Will return 2 packages
+        // 1) sensor PropertyChagned signal emissions
+        // 2) everything else
+        let preproc = Preprocess_DBusPcap(packets, timestamps);
+
+        let grouped = Group_DBus(preproc, v.GroupBy);
+        GenerateTimeLine_DBus(grouped);
+
+        dbus_timeline_view.IsCanvasDirty = true;
+        if (dbus_timeline_view.IsEmpty() == false ||
+            ipmi_timeline_view.IsEmpty() == false) {
+          dbus_timeline_view.CurrentFileName = file_name;
+          ipmi_timeline_view.CurrentFileName = file_name;
+          HideWelcomeScreen();
+          ShowDBusTimeline();
+          ShowIPMITimeline();
+          ShowNavigation();
+          UpdateFileNamesString();
+        }
+        HideBlocker();
+      });
+    });
+  })
+}
+
+// Input: data and timestamps obtained from 
+// Output: Two arrays
+//   The first is sensor PropertyChanged emissions only
+//   The second is all other DBus message types
+//
+// This function also determines the starting timestamp of the capture
+//
+function Preprocess_DBusPcap(data, timestamps) {
+  // Also clear IPMI entries
+  g_ipmi_parsed_entries = [];
+
+  let ret = [];
+
+  let in_flight = {};
+  let in_flight_ipmi = {};
+
+  for (let i = 0; i < data.length; i++) {
+    const packet = data[i];
+
+    // Fields we are interested in
+    const fixed_header = packet[0];  // is an [Array(5), Array(6)]
+
+    if (fixed_header == undefined) {  // for hacked dbus-pcap
+      console.log(packet);
+      continue;
+    }
+
+    const payload = packet[1];
+    const ty = fixed_header[0][1];
+    let timestamp = timestamps[i];
+    let timestamp_end = undefined;
+    const IDX_TIMESTAMP_END = 8;
+    const IDX_MC_OUTCOME = 9;  // Outcome of method call
+
+    let serial, path, member, iface, destination, sender, signature = '';
+    // Same as the format of the Dummy data set
+
+    switch (ty) {
+      case 4: {  // signal
+        serial = fixed_header[0][5];
+        path = fixed_header[1][0][1];
+        iface = fixed_header[1][1][1];
+        member = fixed_header[1][2][1];
+        // signature = fixed_header[1][3][1];
+        // fixed_header[1] can have variable length.
+        // For example: signal from org.freedesktop.PolicyKit1.Authority can
+        // have only 4 elements, while most others are 5
+        const idx = fixed_header[1].length - 1;
+        sender = fixed_header[1][idx][1];
+
+        // Ugly fix for:
+        if (sender == "s" || sender == "sss") {
+          sender = packet[1][0];
+          if (fixed_header[1].length == 6) {
+            // Example: fixed_header is
+            // 0: (2) [7, "org.freedesktop.DBus"]
+            // 1: (2) [6, ":1.1440274"]
+            // 2: (2) [1, "/org/freedesktop/DBus"]
+            // 3: (2) [2, "org.freedesktop.DBus"]
+            // 4: (2) [3, "NameLost"]
+            // 5: (2) [8, "s"]
+            path = fixed_header[1][2][1];
+            iface = fixed_header[1][3][1];
+            member = fixed_header[1][4][1];
+          } else if (fixed_header[1].length == 5) {
+            // Example:  fixed_header is
+            // 0: (2) [7, "org.freedesktop.DBus"]
+            // 1: (2) [1, "/org/freedesktop/DBus"]
+            // 2: (2) [2, "org.freedesktop.DBus"]
+            // 3: (2) [3, "NameOwnerChanged"]
+            // 4: (2) [8, "sss"]
+            path = fixed_header[1][1][1];
+            iface = fixed_header[1][2][1];
+            member = fixed_header[1][3][1];
+          }
+        }
+
+
+        destination = '<none>';
+        timestamp_end = timestamp;
+        let entry = [
+          'sig', timestamp, serial, sender, destination, path, iface, member,
+          timestamp_end, payload
+        ];
+
+        // Legacy IPMI interface uses signal for IPMI request
+        if (iface == 'org.openbmc.HostIpmi' && member == 'ReceivedMessage') {
+          console.log('Legacy IPMI interface, request');
+        }
+
+        ret.push(entry);
+        break;
+      }
+      case 1: {  // method call
+        serial = fixed_header[0][5];
+        path = fixed_header[1][0][1];
+        member = fixed_header[1][1][1];
+        iface = fixed_header[1][2][1];
+        destination = fixed_header[1][3][1];
+        if (fixed_header[1].length > 5) {
+          sender = fixed_header[1][5][1];
+          signature = fixed_header[1][4][1];
+        } else {
+          sender = fixed_header[1][4][1];
+        }
+        let entry = [
+          'mc', timestamp, serial, sender, destination, path, iface, member,
+          timestamp_end, payload, ''
+        ];
+
+        // Legacy IPMI interface uses method call for IPMI response
+        if (iface == 'org.openbmc.HostIpmi' && member == 'sendMessge') {
+          console.log('Legacy IPMI interface, response')
+        } else if (
+            iface == 'xyz.openbmc_project.Ipmi.Server' && member == 'execute') {
+          let ipmi_entry = {
+            netfn: payload[0],
+            lun: payload[1],
+            cmd: payload[2],
+            request: payload[3],
+            start_usec: MyFloatMillisToBigIntUsec(timestamp),
+            end_usec: 0,
+            response: []
+          };
+          in_flight_ipmi[serial] = (ipmi_entry);
+        }
+
+
+        ret.push(entry);
+        in_flight[serial] = (entry);
+        break;
+      }
+      case 2: {  // method reply
+        let reply_serial = fixed_header[1][0][1];
+        if (reply_serial in in_flight) {
+          let x = in_flight[reply_serial];
+          delete in_flight[reply_serial];
+          x[IDX_TIMESTAMP_END] = timestamp;
+          x[IDX_MC_OUTCOME] = 'ok';
+        }
+
+        if (reply_serial in in_flight_ipmi) {
+          let x = in_flight_ipmi[reply_serial];
+          delete in_flight_ipmi[reply_serial];
+          if (payload[0] != undefined && payload[0][4] != undefined) {
+            x.response = payload[0][4];
+          }
+          x.end_usec = MyFloatMillisToBigIntUsec(timestamp);
+          g_ipmi_parsed_entries.push(x);
+        }
+        break;
+      }
+      case 3: {  // error reply
+        let reply_serial = fixed_header[1][0][1];
+        if (reply_serial in in_flight) {
+          let x = in_flight[reply_serial];
+          delete in_flight[reply_serial];
+          x[IDX_TIMESTAMP_END] = timestamp;
+          x[IDX_MC_OUTCOME] = 'error';
+        }
+      }
+    }
+  }
+
+  if (g_ipmi_parsed_entries.length > 0) UpdateLayout();
+  return ret;
+}
