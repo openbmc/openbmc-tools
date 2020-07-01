b65280ff745c47dbb1e668e68e156bf3de6b6e93
Sui Chen <suichen@google.com>
2020-06-30 18:14:03 -0700

    dbus-vis: Initial commit
    dbus-vis visualizes PCAP files captured by busctl as well as
    text-format outputs from busctl.
    
    dbus-vis displays the visualized results in horizontal
    timelines, where each horizontal bar represents a DBus request, or
    an IPMI request (an IPMI request is a specific kind of DBus request
    sent to the IPMI daemon.)
    The requests may be grouped by one or more fields including
    type, sender, destination, path, interface, member.
    
    In addition, if the BMC console is available, dbus-vis can connect
    to the BMC console and capture DBus/IPMI requests in real time.
    
    dbus-vis is in the form of a web page and is wrapped using Electron
    to run as a desktop app.
    
    Basic usage:
    * Mouse click and drag:
      * If mouse is over time axis: drag timeline horizontally
      * If mouse is over vertical scroll bar: scroll up/down
      * Otherwise: select a region for measuring time & zooming in
    * Mouse Scroll wheel:
      * If mouse is over time axis: zoom in/out
      * Otherwise: scroll up/down
    * Click on the expandable headers will expand/collapse the entries under
    that particular header.
    
    Signed-off-by: Sui Chen <suichen@google.com>
    Change-Id: I863c2ba80025d10efb44fd12868e37912fae9a47

---
+++ dbus-vis/renderer.js
@@ -0,0 +1,56 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+
+class Renderer {
+  constructor() {
+    let c1 = document.getElementById('my_canvas_ipmi');
+    let c2 = document.getElementById('my_canvas_dbus');
+    let c3 = document.getElementById('my_canvas_boost_asio_handler');
+    this.canvas1 = c1;
+    this.canvas2 = c2;
+    this.canvas3 = c3;
+    this.width1 = c1.width; this.height1 = c1.height;
+    this.width2 = c2.width; this.height2 = c2.height;
+    this.width3 = c3.width; this.height3 = c3.height;
+    this.ctx1 = this.canvas1.getContext('2d');
+    this.ctx2 = this.canvas2.getContext('2d');
+    this.ctx3 = this.canvas3.getContext('2d');
+    this.frame_count = 0;
+    this.addBindings();
+    this.addListeners();
+    this.update();
+    this.run();
+  }
+
+  addBindings() {
+    this.update = this.update.bind(this);
+    this.run = this.run.bind(this);
+  }
+
+  addListeners() {
+    window.addEventListener('resize', this.update);
+  }
+
+  update() {
+    console.log('update, ' + window.innerWidth + ' x ' + window.innerHeight);
+    if (false) {
+      this.width1 = window.innerWidth;
+      this.height1 = window.innerHeight;
+      this.canvas1.width = this.width1;
+      this.canvas1.height = this.height1;
+    }
+  }
+
+  run() {
+    draw_timeline(this.ctx1);
+    draw_timeline_dbus(this.ctx2);
+    draw_timeline_boost_asio_handler(this.ctx3);
+    window.requestAnimationFrame(this.run);
+  }
+}
+
+g_renderer = new Renderer();
