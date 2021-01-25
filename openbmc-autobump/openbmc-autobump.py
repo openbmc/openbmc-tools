openbmc/openbmc-tools

commit d125d76eabe1484766135c34c8d6e9e6f681acb7

autobump: set topic per new gerrit
Gerrit was upgraded and appears to require the topic now be set via the
topic parameter.

Code as-is started giving this error when running script:

remote: Processing changes: refs: 1, done
To ssh://openbmc.gerrit/openbmc/meta-intel
 ! [remote rejected] HEAD -> refs/for/master/autobump (branch master/autobump not found)
error: failed to push some refs to 'ssh://openbmc.gerrit/openbmc/meta-intel'

Tested:
- Verified script properly updated gerrit with autobump topic

Change-Id: I4a68124c324392c3efcf7683b81ac5152aa8a3d9
Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

openbmc-autobump/openbmc-autobump.py
@@ -174,7 +174,7 @@ def find_and_process_bumps(meta, args):
 
         git.commit(sh.echo(commit_msg), '-s', '-F', '-', _cwd=meta)
 
-        push_args = ['origin', 'HEAD:refs/for/{}/autobump'.format(args.branch)]
+        push_args = ['origin', 'HEAD:refs/for/{}%topic=autobump'.format(args.branch)]
         if not args.dry_run:
             git.push(*push_args, _cwd=meta)
 

commit 199f7959b52388d16a9e03a36d4dd0b6ae441c31

autobump: style fix
Fix an instance of:
  E501: line too long (85 > 79 characters)

Fixes: d125d76eabe1484766135c34c8d6e9e6f681acb7
Change-Id: I8d7e9e6c93491762b5e1eb0fcbe7c207f2f37f5d
Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

openbmc-autobump/openbmc-autobump.py
@@ -174,7 +174,10 @@ def find_and_process_bumps(meta, args):
 
         git.commit(sh.echo(commit_msg), '-s', '-F', '-', _cwd=meta)
 
-        push_args = ['origin', 'HEAD:refs/for/{}%topic=autobump'.format(args.branch)]
+        push_args = [
+            'origin',
+            'HEAD:refs/for/{}%topic=autobump'.format(args.branch)
+        ]
         if not args.dry_run:
             git.push(*push_args, _cwd=meta)
 