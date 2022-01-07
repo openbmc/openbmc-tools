7818b1083b1f6266a8a6761d3554f1c1f29c0270
Patrick Williams <patrick@stwcx.xyz>
2022-01-06 20:26:26 -0600

    tof-voters: add commit analysis subcommand
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I6e56202c014a52d16773415edf59d4e6a7ecdf59

---
+++ tof-voters/libvoters/acceptable.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+import re
+
+
+def project(name: str) -> bool:
+    reject_regex = [
+        ".*-oem",
+        "openbmc/ibm-.*",
+        "openbmc/intel-.*",
+        "openbmc/openpower-.*",
+        "openbmc/google-.*",
+    ]
+
+    reject_repo = [
+        "openbmc/s2600wf-misc",
+    ]
+
+    for r in reject_repo:
+        if r == name:
+            return False
+
+    for r in reject_regex:
+        if re.match(r, name):
+            return False
+
+    return True
+
+
+def file(proj: str, filename: str) -> bool:
+    reject_regex = {
+        "all": [
+            ".*/google/",
+            ".*/ibm/",
+            ".*/intel/",
+            "MAINTAINERS",
+            "OWNERS",
+            "ibm-.*",
+            "ibm_.*",
+        ],
+        "openbmc/entity-manager": ["configurations/.*"],
+        "openbmc/openbmc": ["meta-(?!phosphor).*", "poky/.*"],
+        "openbmc/openbmc-test-automation": ["oem/.*", "openpower/.*"],
+        "openbmc/phosphor-debug-collector": ["dump-extensions/.*"],
+        "openbmc/phosphor-fan-presence": [".*/config_files/.*"],
+        "openbmc/phosphor-logging": [".*/openpower-pels/.*"],
+    }
+
+    reject_files = ["/COMMIT_MSG"]
+
+    for r in reject_files:
+        if r == filename:
+            return False
+
+    for r in reject_regex.get(proj, []) + reject_regex.get("all", []):
+        if re.match(r, filename):
+            return False
+
+    return True
