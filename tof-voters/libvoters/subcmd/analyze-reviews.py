d1d8a6755678f1455a4edd768b84f7abe4bcee0f
Ed Tanous <edtanous@google.com>
2022-02-22 14:03:31 -0800

    Add review ratio to tof tool
    OpenBMC in general struggles to get enough code reviewers.  In an effort
    quantify this, add a new feature to the tof scoring tool to measure each
    individuals "review ratio".
    
    Review ratio is defined as the number of reviews for which an individual
    was given feedback, compared to the number of reviews in which the
    individual gave feedback.  In a well organized system, a majority of
    individuals would have ratios over 1.0, as that implies that for every
    change that you pushed that required a review, you also reviewed someone
    elses.
    
    This is an admittedly noisy measurement when done on a per review basis,
    given that a single review might have 40 patchsets that were reviewed
    before it was merged, but in terms of an abstraction, this is probably
    good enough for the time being.  To reduce the noise, any reviews from
    jenkins are also specifically removed from the counts, given that they
    weren't human effort to do the review/compile/static analysis.
    
    Functionally, this changes the reviews.json structure from
    user->list[reviews]
    to
    user->from_others->list[reviews]
        ->for_others->list[reviews]
    
    Sadly, the report I ran for the last 6 months shows that 137 of the
    196 developers have a ratio of 0, which means that they ONLY push
    patches and don't review anything.  Measuring a problem is a good first
    step to try to improve the situation.
    
    Tested:
    Ran the commands in the readme, and saw review_ratio show up.  Looked up
    a few representative cases, and the results seemed sane.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: I99521361ff6d2e379b5d3ce15fb8380f21b6fb81

--- tof-voters/libvoters/subcmd/analyze-reviews.py
+++ tof-voters/libvoters/subcmd/analyze-reviews.py
@@ -35,7 +35,7 @@ class subcmd:
         before = timestamp(args.before, TimeOfDay.AM)
         after = timestamp(args.after, TimeOfDay.PM)
 
-        changes_per_user: Dict[str, list[int]] = defaultdict(list)
+        changes_per_user = defaultdict(lambda: defaultdict(list))
 
         for f in sorted(os.listdir(args.dir)):
             path = os.path.join(args.dir, f)
@@ -55,7 +55,11 @@ class subcmd:
             if not acceptable.project(project):
                 print("Rejected project:", project, id_number)
 
+            # Comments user left
             comments_per_user: Dict[str, int] = defaultdict(int)
+
+            # Changes for which others left reviews
+            others_comments_per_user: Dict[str, int] = defaultdict(int)
 
             for patch_set in data["patchSets"]:
                 created_on = data["createdOn"]
@@ -65,23 +69,28 @@ class subcmd:
 
                 if "comments" not in patch_set:
                     continue
-
+                patchset_comments = 0
                 for comment in patch_set["comments"]:
                     reviewer = comment["reviewer"]["username"]
 
-                    if reviewer == author:
+                    if reviewer == author or reviewer == "jenkins-openbmc-ci":
                         continue
                     if not acceptable.file(project, comment["file"]):
                         continue
 
                     comments_per_user[reviewer] += 1
+                    others_comments_per_user[author] += 1
+                    patchset_comments += 1
 
             print(project, id_number)
             for (user, count) in comments_per_user.items():
                 if count < 3:
                     continue
                 print("    ", user, count)
-                changes_per_user[user].append(id_number)
+                changes_per_user[user]["for_others"].append(id_number)
+
+            for (user, count) in others_comments_per_user.items():
+                changes_per_user[user]["from_others"].append(id_number)
 
         with open(os.path.join(args.dir, "reviews.json"), "w") as outfile:
             outfile.write(json.dumps(changes_per_user, indent=4))
