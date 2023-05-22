e75a172dbb129406b6e22c0caa11fa0ad8ea8a08
Andrew Jeffery <andrew@aj.id.au>
2023-05-22 12:38:59 +0930

    tof-voters: Record name and email in analysis
    With this we can make the data a little easier to consume.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Change-Id: Id1cc858dc002adfdf35e2a7bb790dc4872d9c570

--- tof-voters/libvoters/__init__.py
+++ tof-voters/libvoters/__init__.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+from typing import TypedDict
+
+UserChanges = TypedDict('User', {'name': str, 'email': str, 'changes': list[int]})
+def changes_factory():
+    return {"name": None, "email": None, "changes": list()}
+
+UserComments = TypedDict('User', {'name': str, 'email': str, 'comments': int})
+def comments_factory():
+    return {"name": None, "email": None, "comments": 0}
